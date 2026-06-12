fac06b2ee015c6dc515f075b491fe0e12323e617
rahmadhiniagustina-alt <rahmadhiniagustina@gmail.com>
2026-06-12 10:51:32 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -797,7 +797,7 @@ elif menu == "🎮 Kuis":
         belum_diisi = [item["tanya"][:4] for item in soal_list if jawaban_user[item["id"]] is None]
         
         if belum_diisi:
-            st.warning(f"⚠️ Tolong isi semua pertanyaan terlebih dahulu! Nomor yang belum diisi: {', '.join(belum_diisi)}")
+            st.warning("⚠️ Tolong isi semua pertanyaan terlebih dahulu! Nomor yang belum diisi")
             st.session_state.kuis_disubmit = False
         else:
             # Hitung skor akhir
